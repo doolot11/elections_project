c94d86aeacf266ed3321eed9a9704f2049e54fd3
Umar <umar@MacBook-Air-Umar.local>
2024-11-11 23:33:44 +0600

    added 50%

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Box } from "@mui/material";
+import MapTest from "./components/MapTest";
+import InfoBlog from "./components/InfoBlog";
+import { useState } from "react";
+import Carousel from "./components/Carousel";
 
 function App() {
+  const [data,setData] = useState()
+  console.log("main data=",data);
+  
   return (
-    <div className="App">
-    
-    </div>
+
+    <Box>
+      <Box sx={{background:"white",textAlign:"center",padding:"10px"}}>
+        <h1 style={{color:"#20B2AA",fontWeight:"500",fontSize:"35px"}}>Парламент 2024</h1>
+      </Box>
+      <Box>
+        <Carousel/>
+      </Box>
+      <Box sx={{display:"grid",gridTemplateColumns:"80% 20%",alignItems:"start"}}>
+        <MapTest setData={setData}/>
+        <InfoBlog data={data}/>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/components/InfoBlog.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+
+function InfoBlog({ data }) {
+    return (
+        <Box sx={{ margin: "10px" }}>
+            <Box sx={{ background: "white", color: "#66CDAA", padding: "10px" }}>
+                <h2 style={{ fontWeight: "300", fontSize:"20px" }}>{data?.name}</h2>
+            </Box>
+            <Box sx={{display:"flex",flexDirection:"column",gap:"10px",padding:"0 10px"}}>
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", color: "white", marginTop: "10px" }}>
+                    <p>Атажурт</p>
+                    <p>100%</p>
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", color: "white", marginTop: "10px" }}>
+                    <p>Атажурт</p>
+                    <p>100%</p>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
+
+export default InfoBlog;
