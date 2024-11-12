6c2b344dce770b7fa7ccb120d9a68e4648eca59d
Umar <umar@MacBook-Air-Umar.local>
2024-11-12 20:58:37 +0600

    tete

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
   
   return (
 
-    <Box>
+    <Box sx={{display:"flex",flexDirection:"column",gap:"auto"}}>
       <Box sx={{background:"white",textAlign:"center",padding:"10px"}}>
         <h1 style={{color:"#20B2AA",fontWeight:"500",fontSize:"35px"}}>Парламент 2024</h1>
       </Box>
-      <Box>
+      <Box sx={{width:"80%",maxWidth:"1200px",margin:"0 auto"}}>
         <Carousel/>
       </Box>
-      <Box sx={{display:"grid",gridTemplateColumns:"80% 20%",alignItems:"start"}}>
+      <Box sx={{display:"grid",gridTemplateColumns:"80% 20%",alignItems:"start",width:"80%",maxWidth:"1200px",margin:"0 auto"}}>
         <MapTest setData={setData}/>
         <InfoBlog data={data}/>
       </Box>
